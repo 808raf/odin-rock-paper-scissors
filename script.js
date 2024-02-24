2227316cb0e804033c2edb1ffc84f5e28fa71d4b
808raf <102700868+808raf@users.noreply.github.com>
2024-02-24 13:30:29 -1000

    Add functionality to buttons
    Previously nothing would happen when buttons were pressed. Now each
    button plays a game of rock paper scissors in the console.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let userScore = 0;
 let computerScore = 0;
+let playerSelection = "";
+let computerSelection = "";
 
 const options = ["rock", "paper", "scissors"]
 
 function addUserScore(){
-    userScore++;
+    return userScore++;
 }
 
 function addComputerScore(){
-    computerScore++;
+    return computerScore++;
 }
 
 function checkWinner(userScore, computerScore){
@@ -21,37 +23,26 @@ function checkWinner(userScore, computerScore){
     }
 }
 
-// Function to randomly return rock, paper, or scissors for the computers choice
 const getComputerChoice = () => {
     let choice = 0;
     choice = Math.floor(Math.random() * 3)
     return options[choice]
 }
 
-// Function to play one round
 function playRound(playerSelection, computerSelection) {
-    // Take player selection and make it all lowercase
-    playerSelection = playerSelection.toLowerCase();
 
-    // End of round messages
     const win = `You win! You picked ${playerSelection} and the computer picked ${computerSelection}.`
     const lose = `You lose! You picked ${playerSelection} and the computer picked ${computerSelection}.`
     const tie = `It's a tie! You picked ${playerSelection} and the computer picked ${computerSelection}.`
     let result = "";
 
-    // Check to see if the choice is valid
-    if (!options.includes(playerSelection)){
-        return "Please make a valid selection."
-    }
     // Conditionals for winning
     if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper"){
         result = win;
         addUserScore();
-        // If they have the same choice return a tie
     } else if (playerSelection == computerSelection ){
         result = tie;
     } else {
-        // If its not a win or a tie, its a lose
         result = lose;
         addComputerScore();
     }
@@ -60,34 +51,40 @@ function playRound(playerSelection, computerSelection) {
   }
 
 function playGame() {
-    
-    for(let i = 0;i < 5;i++){
-        const playerSelection = prompt("Rock, paper, or scissors?");
-        const computerSelection = getComputerChoice();
-
         console.log(playRound(playerSelection, computerSelection));
 
-        console.log(`SCORES (Round ${i + 1} of 5):
+        console.log(`SCORES:
         YOU: ${userScore}
         COMPUTER: ${computerScore}`)
-    }
 
     checkWinner(userScore,computerScore);
 }
-//Select div with id of main
+
 const mainDiv = document.querySelector("#main")
 
-//Create 3 buttons
 const rockBtn = document.createElement("button");
 const paperBtn = document.createElement("button");
 const scissorsBtn = document.createElement("button");
 
-//Change names for each button
 rockBtn.textContent = "Rock";
+rockBtn.classList.add("btn");
 paperBtn.textContent = "Paper";
+paperBtn.classList.add("btn");
 scissorsBtn.textContent = "Scissors";
+scissorsBtn.classList.add("btn");
 
-//Attach buttons to div with id of main
 mainDiv.append(rockBtn, paperBtn, scissorsBtn);
 
-console.log(playGame());
+const buttons = document.querySelectorAll(".btn")
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        playerSelection = e.target.innerHTML.toLowerCase();
+        computerSelection = getComputerChoice();
+        return console.log(playGame())
+    })
+})
+
+const scoreDiv = document.querySelector("#score");
+
+const displayUserScore = document.createElement("span");
+const displayComputerScore = document.createElement("span");
